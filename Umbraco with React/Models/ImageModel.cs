feeb0021d3b39e6267a2c6f60261673e9d3fa7fc
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 22:53:56 +0100

    create image model and model adapter

---
+++ Umbraco with React/Models/ImageModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco_with_React.Models
+{
+    public class ImageModel
+    {
+        public string Source { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string AltText { get; set; }
+        public string Url { get; set; }
+        public string Target { get; set; }
+    }
+}
