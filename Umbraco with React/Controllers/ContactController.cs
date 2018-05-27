4f8a05434ea30417a229306df9a073d4adc431f6
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 18:40:13 +0100

    create conatct controller with relative model

---
+++ Umbraco with React/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Umbraco.Core.Models;
+using Umbraco_with_React.Models;
+using Umbraco_with_React.Model_Adapter.Interafaces;
+
+namespace Umbraco_with_React.Controllers
+{
+    public class ContactController:DefaultController
+    {
+        private readonly IModelAdapter<ContactModel> _contactModelAdapter;
+
+        public ContactController(IModelAdapter<MasterModel> masterModelAdapter,
+            IModelAdapter<InitialState> initialStateModelAdapter,
+            IModelAdapter<ContactModel> contactModelAdapter) : base(masterModelAdapter, initialStateModelAdapter)
+        {
+            _contactModelAdapter = contactModelAdapter;
+        }
+        protected override InitialState CreateInitialState(IPublishedContent content)
+        {
+            var contactModel=_contactModelAdapter.Adapt(content);
+            var initialState = InitialStateModelAdapter.Adapt(content);
+            initialState.Content = contactModel;
+            return initialState;
+        }
+        
+        
+    }
+}
