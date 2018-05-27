b3622b33d727037d3c667b54dbf0f7ae58ec5944
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 20:32:52 +0100

    add properties to blogpost model and create blogpost model adapter

---
+++ Umbraco with React/Model Adapter/BlogPostModelAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco_with_React.Models;
+using Umbraco_with_React.Model_Adapter.Interafaces;
+
+namespace Umbraco_with_React.Model_Adapter
+{
+    public class BlogPostModelAdapter:IModelAdapter<BlogPostModel>
+    {
+        public BlogPostModel Adapt(IPublishedContent content)
+        {
+            return new BlogPostModel
+            {
+                PageTitle = content.GetPropertyValue<string>("pageTitle"),
+                Categories = content.GetPropertyValue<IEnumerable<string>>("categories"),
+                Excerpt = content.GetPropertyValue<string>("excerpt"),
+                Content = content.GetPropertyValue<string>("bodyText"),
+                CreatedData = content.CreateDate.ToShortDateString()
+            };
+        }
+    }
+}
