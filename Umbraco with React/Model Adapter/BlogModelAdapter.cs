5ac54f99d0cc1aaa00d574395d6bc23721ae0d20
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 20:44:05 +0100

    create model adapter for blogModel and add properties to blog model

---
+++ Umbraco with React/Model Adapter/BlogModelAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco_with_React.Models;
+using Umbraco_with_React.Model_Adapter.Interafaces;
+
+namespace Umbraco_with_React.Model_Adapter
+{
+    public class BlogModelAdapter: IModelAdapter<BlogModel>
+    {
+        private readonly IModelAdapter<BlogPostModel> _blodPostModelAdapter;
+
+        public BlogModelAdapter(IModelAdapter<BlogPostModel> blodPostModelAdapter)
+        {
+            _blodPostModelAdapter = blodPostModelAdapter;
+        }
+
+        public BlogModel Adapt(IPublishedContent content)
+        {
+            var numberOfBlopostToShown = content.GetPropertyValue<int>("howManyPostsShouldBeShown");
+            var blogPost=content.Children.OrderByDescending(x => x.CreateDate).Take(numberOfBlopostToShown)
+                .Select(y=>_blodPostModelAdapter.Adapt(y));
+            
+            return new BlogModel
+            {
+                BlogPost=blogPost
+            };
+        }
+    }
+}
