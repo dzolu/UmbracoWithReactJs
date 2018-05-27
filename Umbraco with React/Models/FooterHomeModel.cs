230c2bb5b40cbc8e345dce3e2bf4fa022e15a191
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 22:48:09 +0100

    create footerHome model and model adapter

---
+++ Umbraco with React/Models/FooterHomeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Umbraco_with_React.Models
+{
+    public class FooterHomeModel
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public string CallToActionCaption { get; set; }
+        public string CallToActionLink { get; set; }
+        public string Address { get; set; }
+    }
+}
