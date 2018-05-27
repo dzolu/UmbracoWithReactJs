89183222211f8222aafb9c4504914f0befc1bbab
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 22:49:23 +0100

    create hero model and model adapter

---
+++ Umbraco with React/Models/HeroModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Umbraco_with_React.Models
+{
+    public class HeroModel
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public string CallToActionCaption { get; set; }
+        public string CallToActionLink { get; set; }
+    }
+}
