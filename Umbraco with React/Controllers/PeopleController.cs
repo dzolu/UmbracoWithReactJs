32d540f0b6eaf85a59d21f922bfc942ffa7e1fc4
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 18:50:47 +0100

    create people controller with relative model

---
+++ Umbraco with React/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using Umbraco.Core.Models;
+using Umbraco_with_React.Models;
+using Umbraco_with_React.Model_Adapter.Interafaces;
+
+namespace Umbraco_with_React.Controllers
+{
+    public class PeopleController : DefaultController
+    {
+        private readonly IModelAdapter<PeopleModel> _peopleModelAdapter;
+
+        public PeopleController(IModelAdapter<MasterModel> masterModelAdapter,
+            IModelAdapter<InitialState> initialStateModelAdapter,
+            IModelAdapter<PeopleModel> peopleModelAdapter) : base(masterModelAdapter, initialStateModelAdapter)
+        {
+            _peopleModelAdapter = peopleModelAdapter;
+        }
+        
+        protected override InitialState CreateInitialState(IPublishedContent content)
+        {
+            var peopleModel = _peopleModelAdapter.Adapt(content);
+            var initialState = InitialStateModelAdapter.Adapt(content);
+            initialState.Content = peopleModel;
+            return initialState;
+        }
+    }
+}
