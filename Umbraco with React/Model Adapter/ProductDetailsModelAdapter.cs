c248f0cd60c4a216ba2c1929de454d7d82f7b2cd
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-27 22:54:25 +0100

    create product details model and model adapter

---
+++ Umbraco with React/Model Adapter/ProductDetailsModelAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco_with_React.Models;
+using Umbraco_with_React.Model_Adapter.Interafaces;
+
+namespace Umbraco_with_React.Model_Adapter
+{
+    public class ProductDetailsModelAdapter:IModelAdapter<ProductDetailsModel>
+    {
+        private readonly IModelAdapter<ImageModel> _imageModelAdapter;
+
+        public ProductDetailsModelAdapter(IModelAdapter<ImageModel> imageModelAdapter)
+        {
+            _imageModelAdapter = imageModelAdapter;
+        }
+
+        public ProductDetailsModel Adapt(IPublishedContent content)
+        {
+            return new ProductDetailsModel
+            {
+                Name = content.GetPropertyValue<string>("productName"),
+                Price = content.GetPropertyValue<double>("price"),
+                Category = content.GetPropertyValue<IEnumerable<string>>("category"),
+                Description = content.GetPropertyValue<string>("description"),
+                Sku = content.GetPropertyValue<string>("sku"),
+                Photos = content.GetPropertyValue<IEnumerable<IPublishedContent>>("photos").Select(x=>_imageModelAdapter.Adapt(x))
+                
+            };
+        }
+    }
+}
---
+++ Umbraco with React/Models/ProductDetailsModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Umbraco_with_React.Models
+{
+    public class ProductDetailsModel
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public IEnumerable<string> Category { get; set; }
+        public string Description { get; set; }
+        public string Sku { get; set; }
+        public IEnumerable<ImageModel> Photos { get; set; }
+    }
+}
