f3ff8c70cd31eb5423ee6c5dd9ba950a8b4d840f
Tomasz Koszkul <tomasz.koszkul@gmail.com>
2018-05-28 15:52:43 +0100

    remane project

--- UmbracoWithReactJs/Properties/AssemblyInfo.cs
+++ UmbracoWithReactJs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Umbraco_with_React")]
+[assembly: AssemblyTitle("UmbracoWithReactJs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Umbraco_with_React")]
+[assembly: AssemblyProduct("UmbracoWithReactJs")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
